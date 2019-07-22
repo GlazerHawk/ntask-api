9d3414b8ca384a3a5e87f967f0043febdd0a2991
Mayk Moreira <mayk.hsmdeveloper@gmail.com>
2019-07-22 13:29:26 -0300

    Initial Commit

---
+++ ntask-api/index.js
@@ -0,0 +1,6 @@
+let express = require('express');
+let consign = require('consign');
+const app = express();
+
+consign().include("models").then("libs/middlewares.js").then("routes").then("libs/boot.js").into(app);
+
